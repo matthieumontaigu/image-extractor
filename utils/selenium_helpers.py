a3203c257175855aa634b6b7f87f3f61831d673b
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-07 15:40:32 +0200

    Refactor imports in selenium_helpers.py for better organization

--- utils/selenium_helpers.py
+++ utils/selenium_helpers.py
@@ -1,12 +1,8 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-
-
 def get_chrome_driver():
     """Set up Chrome options to run headless (= without opening a browser window)"""
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+
     chrome_options = Options()
     chrome_options.add_argument("--headless")
 
@@ -17,5 +13,7 @@ def get_chrome_driver():
 
 
 def click_button(driver, button_xpath):
+    from selenium.webdriver.common.by import By
+
     button = driver.find_element(By.XPATH, button_xpath)
     button.click()
