6d0c5bf4e5d9066a1c8e28a5aaf3423cce9e06c2
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-07 15:40:26 +0200

    Add type hints to URL helper functions

--- utils/url_helpers.py
+++ utils/url_helpers.py
@@ -2,7 +2,7 @@ import re
 from urllib.parse import urlparse
 
 
-def clean_url(url):
+def clean_url(url: str) -> str:
     """
     URLs from Apple TV Mac contains parameters that need to be removed (split ?)
     URLS from Safari macOS are doubled, take the first one (split \n)
@@ -11,15 +11,15 @@ def clean_url(url):
     return url
 
 
-def extract_domain(url):
+def extract_domain(url: str) -> str:
     return urlparse(url).netloc
 
 
-def get_movie_path(url):
+def get_movie_path(url: str) -> str | None:
     match = re.search(r"tv.apple.com(.+)$", url)
     return match.groups()[0] if match else None
 
 
-def get_resized_image_url(url, size):
+def get_resized_image_url(url: str, size: str) -> str:
     """Example: size = 3840x2160.jpg"""
     return re.sub(r"[\w]+x[\w]+\.jpg$", size, url)
