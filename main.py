c09b6e4fbe52df61070ff79782e555647332b7e1
matthieumontaigu <matthieu.montaigu@icloud.com>
2025-04-07 16:52:39 +0200

    Refactor main function to include thumbnail option in extract command and update extract logic to use new artwork extraction methods

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import argparse
 
-from services.apple_tv_service import scrape_apple_tv_image
-from services.itunes_service import get_itunes_movies_images, scrape_itunes_image
-from utils.print_helpers import print_extract_results, print_search_results
+from services.apple_tv.extract import extract_artworks
+from services.itunes.extract import extract_artwork, get_artworks
+from utils.print_helpers import print_artworks, print_search_results
 from utils.url_helpers import clean_url, extract_domain, get_movie_path
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description="Fetch movie posters")
     subparsers = parser.add_subparsers(dest="command", help="Available subcommands")
 
@@ -14,6 +14,11 @@ def main():
         "extract", help="Get movie poster URL from Apple TV and iTunes"
     )
     extract_parser.add_argument("--url", help="URL of the movie")
+    extract_parser.add_argument(
+        "--thumbnail",
+        action="store_true",
+        help="Get the thumbnail image of the movie",
+    )
     extract_parser.add_argument(
         "--use-selenium",
         action="store_true",
@@ -28,12 +33,12 @@ def main():
 
     args = parser.parse_args()
     if args.command == "extract":
-        return extract(args.url, args.use_selenium)
+        return extract(args.url, args.thumbnail, args.use_selenium)
     if args.command == "search":
         return search(args.country, args.term)
 
 
-def extract(url, use_selenium):
+def extract(url: str, thumbnail: bool, use_selenium: bool) -> None:
     url = clean_url(url)
     domain = extract_domain(url)
 
@@ -41,13 +46,20 @@ def extract(url, use_selenium):
         raise ValueError(f"unsupported domain {domain}")
 
     movie_path = get_movie_path(url)
-    itunes_image = scrape_itunes_image(movie_path)
-    apple_tv_image = scrape_apple_tv_image(url, movie_path, use_selenium)
-    print_extract_results(itunes_image, apple_tv_image)
+
+    artworks = extract_artworks(
+        url, thumbnail=thumbnail, movie_path=movie_path, use_selenium=use_selenium
+    )
+
+    title, poster_url = extract_artwork(movie_path)
+    if poster_url:
+        artworks["poster"] = poster_url
+
+    print_artworks(title, artworks)
 
 
-def search(country, term):
-    movies_images = get_itunes_movies_images(country, term, how_many=10)
+def search(country: str, term: str) -> None:
+    movies_images = get_artworks(country, term, how_many=10)
     print_search_results(movies_images)
 
 
